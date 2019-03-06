a24adf9a43ef9c9d079ce89a86130a9ee32fb3c0
Edhuardo Tierrez <edhuardo.tierrez@gmail.com>
2019-03-06 19:37:01 -0300

    Fixing Dummy.

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from '../config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('application', {path: '/*'});
 });
 
 export default Router;
